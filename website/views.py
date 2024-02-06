8d7936319bddda0d2e8b24a166b5314a88a529b7
Aditya Raj <adistrim.dev@gmail.com>
2024-02-07 02:07:15 +0530

    can logout now

--- website/views.py
+++ website/views.py
@@ -20,8 +20,7 @@ def home(request):
         
     return render(request, 'home.html', {})
 
-def login_user(request):
-    pass
-
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, ('You have been logged out'))
+    return redirect('home')
