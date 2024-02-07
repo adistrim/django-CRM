3e6141c7f8831bf42a8770a4e9ef038ef3693eac
Aditya Raj <adistrim.dev@gmail.com>
2024-02-07 12:29:35 +0530

    can delete records now

--- website/urls.py
+++ website/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('signup/', views.signup_user, name='signup'),
     path('record/<int:pk>', views.customer_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
 ]
