c5891cabd2224c1270e387e235d175aeebaf0f92
Aditya Raj <adistrim.dev@gmail.com>
2024-02-07 13:19:06 +0530

    records can be updated by users

--- website/views.py
+++ website/views.py
@@ -81,3 +81,17 @@ def add_record(request):
         return redirect('home')
     
     return render(request, 'add_record.html', {'form':form})
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('You Just Updated a Record'))
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, ('You must be logged in.'))
+        return redirect('home')
